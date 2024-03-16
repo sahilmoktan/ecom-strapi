4515bd70f02c834d8830502ecf2122b71d87c913
sahilmoktan <moktanshail9@gmail.com>
2024-03-16 21:17:33 +0545

    signup/login UI and data fetch successfully

--- ecomm-client/src/pages/Login.js
+++ ecomm-client/src/pages/Login.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { LOGIN_USER } from "../gqlOperation/mutation";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const [loginUser,{loading, error,data}] = useMutation(LOGIN_USER)
 
-export default Login
+  if(loading) return <h2>loading ...</h2>
+  if(data) console.log(data)
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    loginUser({
+      variables:{
+        input:formData
+      }
+    })
+    
+  };
+
+  const handelChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="container" style={{ maxWidth: "500px" }}>
+      {
+        error &&
+        <div className="card-panel red">{error.message}</div>
+      }
+      <h3> Login</h3>
+      <form onSubmit={handelSubmit}>
+        <input
+          type="text"
+          placeholder="email or username"
+          name="identifier"
+          onChange={handelChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handelChange}
+        />
+        <button type="submit" className="btn blue">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- ecomm-client/src/pages/Signup.js
+++ ecomm-client/src/pages/Signup.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { SIGNUP_USER } from "../gqlOperation/mutation";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const [SignupUser,{loading, error,data}] = useMutation(SIGNUP_USER)
 
-export default Signup
+  if(loading) return <h2>loading ...</h2>
+  if(data) console.log(data)
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    SignupUser({
+      variables:{
+        input:formData
+      }
+    })
+    
+  };
+
+  const handelChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="container" style={{ maxWidth: "500px" }}>
+      {
+        error &&
+        <div className="card-panel red">{error.message}</div>
+      }
+      <h3> Signup</h3>
+      <form onSubmit={handelSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handelChange}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          onChange={handelChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handelChange}
+        />
+        <button type="submit" className="btn blue">
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
