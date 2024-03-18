cebb235ddfbe0dc5db4cc33c26d7e016bc51d714
sahilmoktan <moktanshail9@gmail.com>
2024-03-18 22:58:57 +0545

    database change

--- ecommerce/config/database.js
+++ ecommerce/config/database.js
@@ -1,13 +1,29 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST"),
+//       port: env.int("DATABASE_PORT"),
+//       database: env("DATABASE_NAME"),
+//       user: env("DATABASE_USERNAME"),
+//       password: env("DATABASE_PASSWORD"),
+//       ssl: env.bool("DATABASE_SSL", true),
+//     },
+//   },
+// });
+
+
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'postgres',
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", true),
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      password: env('PGPASSWORD', 'password'),
+      ssl: env.bool(true),
     },
+    pool: { min: 0 }
   },
 });
